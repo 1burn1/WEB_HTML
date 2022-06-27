c22f26b680747d3f310b441700597ce195dc1f75
liuyu <ly1594253944@gmail.com>
2022-06-27 18:43:19 +0800

    master

--- index.php
+++ index.php
@@ -1,11 +1,1 @@
-<!DOCTYPE html>
-<html lang=zh-CN>
-<head>
-    <meta charset="UTF-8">
-    <title>DEFAULT</title>
-</head>
-<body>
-    <a href="./images/QQ图片20220611014012.jpg" download="./images/QQ图片20220611014012.jpg">图片</a>
-    <a href="./scripts.rar">文件</a>
-    <a href="aaa">哈哈哈哈哈</a>
-</html>
+<?php include_once("index.html"); ?>
